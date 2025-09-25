5b193bfbe283d3d518ed30ccb001c9cb0c580a0f
Adrian11Faudoa <afa1823@gmail.com>
2025-09-24 21:09:44 -0600

    Update Structure Code

--- server.js
+++ server.js
@@ -1,56 +1,119 @@
-'use strict';
-
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
-require('dotenv').config();
-
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
-
+'use strict'
+const express = require('express');
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+const cors = require('cors');
+const runner = require('./test-runner');
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
-
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function (req, res) {
+  const name = req.query.name || 'Guest';
+  res.type('txt').send('hello ' + name);
+})
+
+const travellers = function (req, res) {
+  let data = {};
+  if (req.body && req.body.surname) {
+    switch (req.body.surname.toLowerCase()) {
+      case 'polo':
+        data = {
+          name: 'Marco',
+          surname: 'Polo',
+          dates: '1254 - 1324'
+        };
+        break;
+      case 'colombo':
+        data = {
+          name: 'Cristoforo',
+          surname: 'Colombo',
+          dates: '1451 - 1506'
+        };
+        break;
+      case 'vespucci':
+        data = {
+          name: 'Amerigo',
+          surname: 'Vespucci',
+          dates: '1454 - 1512'
+        };
+        break;
+      case 'da verrazzano':
+      case 'verrazzano':
+        data = {
+          name: 'Giovanni',
+          surname: 'da Verrazzano',
+          dates: '1485 - 1528'
+        };
+        break;
+      default:
+        data = {
+          name: 'unknown'
+        }
+    }
+  }
+  res.json(data);
+};
+
+
+app.route('/travellers')
+  .put(travellers);
+
+let error;
+app.get('/_api/get-tests', cors(), function (req, res, next) {
+  if (error)
+    return res.json({ status: 'unavailable' });
+  next();
+},
+  function (req, res, next) {
+    if (!runner.report) return next();
+    res.json(testFilter(runner.report, req.query.type, req.query.n));
+  },
+  function (req, res) {
+    runner.on('done', function (report) {
+      process.nextTick(() => res.json(testFilter(runner.report, req.query.type, req.query.n)));
+    });
   });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Listening on port " + port);
+  console.log('Running Tests...');
+  setTimeout(function () {
+    try {
+      runner.run();
+    } catch (e) {
+      error = e;
+      console.log('Tests are not valid:');
+      console.log(error);
+    }
+  }, 1500);
 });
 
-//Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
-      }
-    }, 1500);
+
+module.exports = app; // for testing
+
+function testFilter(tests, type, n) {
+  let out;
+  switch (type) {
+    case 'unit':
+      out = tests.filter(t => t.context.match('Unit Tests'));
+      break;
+    case 'functional':
+      out = tests.filter(t => t.context.match('Functional Tests'));
+      break;
+    default:
+      out = tests;
   }
-});
-
-module.exports = app; //for unit/functional testing
+  if (n !== undefined) {
+    return out[n] || out;
+  }
+  return out;
+}
